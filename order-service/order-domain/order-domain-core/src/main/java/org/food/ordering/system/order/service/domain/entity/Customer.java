85b8236439189d33c861e8b5c2a7bc7c1c571bb5
Omid Esmailbeig <omid.delta96@gmail.com>
2022-12-17 00:22:12 +0330

    Add Restaurant and Customer entity to Order Service domain

---
+++ order-service/order-domain/order-domain-core/src/main/java/org/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package org.food.ordering.system.order.service.domain.entity;
+
+import org.food.ordering.system.domain.entity.AggregateRoot;
+import org.food.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
